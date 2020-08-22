ba70febea5fb72786d9cdfd83689f53f87a3ef3e
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-22 21:43:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@ def display_rainbow(['red','orange','yellow','green','blue', 'indigo', 'violet']
   
 end
 
-print ('R: 'display_rainbow[0],' 0: 'display_rainbow[1],' Y: 'display_rainbow[2],' G: 'display_rainbow[3],' B: 'display_rainbow[4],' I: 'display_rainbow[5],' V: 'display_rainbow[6])
+display rainbow ('R: 'display_rainbow[0],' 0: 'display_rainbow[1],' Y: 'display_rainbow[2],' G: 'display_rainbow[3],' B: 'display_rainbow[4],' I: 'display_rainbow[5],' V: 'display_rainbow[6])
