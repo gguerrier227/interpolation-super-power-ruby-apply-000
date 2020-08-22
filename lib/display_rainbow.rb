82fc5c2cd87fa0665af61ec2ddcb6c099064861a
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-22 21:44:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  (['red','orange','yellow','green','blue', 'indigo', 'violet'])
+  prints['red','orange','yellow','green','blue', 'indigo', 'violet']
   
 end
 
